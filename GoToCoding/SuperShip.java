e0adcb0104cff8ef7a2099575d59862f8b2fe5c1
Ruzal <ruzal.ibragimov@gmail.com>
2016-06-21 20:10:04 +0300

    Исправил игровую логику. Переделал хранение координат на HashSet

--- GoToCoding/SuperShip.java
+++ GoToCoding/SuperShip.java
@@ -1,12 +1,12 @@
 package GoToCoding;
 
+import java.util.HashSet;
+
 /**
  * Created by User-pc on 21.06.2016.
  */
 public class SuperShip {
-    private int[] place;
-    private boolean[] wasKilled;
-    private int cnt;
+    private HashSet<Integer> place;
     public String Action(String act) {
         String res = "Missed.";
         int x;
@@ -17,31 +17,22 @@ public class SuperShip {
             return "You`ve entered not a number";
         }
 
-        for(int i = 0; i < place.length; i++) {
-            if(place[i] == x) {
-                if(wasKilled[i]) {
-                    res = "You are already shot here! =/";
-                } else {
-                    wasKilled[i] = true;
-                    res = "Right on target!";
-                    cnt++;
-                }
-                break;
-            }
+        if(place.contains(x)) {
+            res = "Right on target!";
+            place.remove(x);
         }
 
-        if(cnt == place.length) {
+        if(place.isEmpty()) {
             res = "Flooded the entire ship!";
         }
 
         return res;
     }
-    public SuperShip(int a) {
-        place = new int[a];
-        wasKilled = new boolean[place.length];
-        int randomX = (int)Math.random() * 5 + 1;
-        for(int i = 0; i < a; i++) {
-            place[i] = randomX + i;
+    public SuperShip(int length) {
+        this.place = new HashSet<>();
+        int randomX = (int) (Math.random() * 5) + 1;
+        for(int i = 0; i < length; i++) {
+            this.place.add(randomX + i);
         }
     }
 }
