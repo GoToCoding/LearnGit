445acf19e5be0ecd59c4b8a5088221101332ec34
Ruzal <ruzal.ibragimov@gmail.com>
2016-06-26 15:44:18 +0300

    Немного изменил приветственное сообщение

--- GoToCoding/Main.java
+++ GoToCoding/Main.java
@@ -3,11 +3,12 @@ package GoToCoding;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        System.out.println("Hi");
 
         BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
 
